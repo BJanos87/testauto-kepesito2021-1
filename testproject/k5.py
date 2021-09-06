2223467e755b199f3a9a5c272791be1f61473c44
Janos Babarik <babarik.janos@gmail.com>
2021-09-06 15:09:34 +0200

    k5.py file is added to github repo (v2)

--- testproject/k5.py
+++ testproject/k5.py
@@ -15,6 +15,7 @@ driver.get(URL)
 cells = driver.find_elements_by_xpath("html/body/div/div/div/table/tbody[@id='bingo-body']/tr/td")
 number_list = driver.find_elements_by_xpath("html/body/div/div/div[3]/ol/li")
 bingo_text = driver.find_element_by_id("messages")
+spin_btn = driver.find_element_by_id("spin")
 
 
 # TC01
@@ -23,6 +24,18 @@ def test_tc01():
     try:
         assert len(cells) == 25
         assert len(number_list) == 75
+    except AssertionError as e:
+        pytest.fail(print("Az oldalon megjelenített karakter nem megfelelő", e))
+
+# TC02
+# Applikáció helyes működésének ellenőrzése
+def test_tc02():
+    try:
+        while bingo_text.is_displayed() is False:
+            spin_btn.click()
+            if bingo_text.is_displayed() is True:
+                break
+        assert bingo_text.text == "BINGO"
     except AssertionError as e:
         pytest.fail(print("Az oldalon megjelenített karakter nem megfelelő", e))
 
