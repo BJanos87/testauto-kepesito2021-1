aa10cdd322fde5a553c34171f0b90148d97764b5
Janos Babarik <babarik.janos@gmail.com>
2021-09-06 15:02:38 +0200

    k5.py file is added to github repo (v1)

---
+++ testproject/k5.py
@@ -0,0 +1,30 @@
+# Beimportálom és beállítom a használandó webdriver-t (Google Chrome)
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+# A weboldal elérésének letárolása egy változóba
+URL = "https://ambitious-sky-0d3acbd03.azurestaticapps.net/k5.html"
+
+# Oldal megnyitása
+driver.get(URL)
+
+# A változókba lokátorok segítségével letárolom a használandó web-element-eket
+cells = driver.find_elements_by_xpath("html/body/div/div/div/table/tbody[@id='bingo-body']/tr/td")
+number_list = driver.find_elements_by_xpath("html/body/div/div/div[3]/ol/li")
+bingo_text = driver.find_element_by_id("messages")
+
+
+# TC01
+# Applikáció helyes megjelenésének ellenőrzése
+def test_tc01():
+    try:
+        assert len(cells) == 25
+        assert len(number_list) == 75
+    except AssertionError as e:
+        pytest.fail(print("Az oldalon megjelenített karakter nem megfelelő", e))
+
+# A megnyitott böngésző bezárásra kerül
+# driver.close()
